cba07fda90728cdc9dc77e7aa7e0f032483684c0
TefiC <tefi.developer59@gmail.com>
2017-09-15 17:04:00 -0400

    refactor: Included helper methods for DetailsActivity
    Also improved existing documentation and format

--- app/src/main/java/com/example/android/favoritemoviesapp/DetailsActivity.java
+++ app/src/main/java/com/example/android/favoritemoviesapp/DetailsActivity.java
@@ -12,17 +12,32 @@ import com.squareup.picasso.Picasso;
 
 public class DetailsActivity extends AppCompatActivity {
 
+    /*
+     * Fields
+     */
+
     ImageView moviePosterView;
     TextView movieVoteAverageView;
     TextView movieReleaseView;
     TextView moviePlotView;
     TextView movieTitleView;
 
+    /*
+     * Constants
+     */
+
+    private static final String NOT_AVAILABLE = "Not available";
+
+    /*
+     * Methods
+     */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
+        // Setting toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -38,7 +53,7 @@ public class DetailsActivity extends AppCompatActivity {
         // Make plot view scrollable
         moviePlotView.setMovementMethod(new ScrollingMovementMethod());
 
-        // Get corresponding movie and fill views with data
+        // Get movie from intent and populate views with data
         if (intentThatStartedThisActivity.hasExtra("movieObject")) {
             Movie movie = intentThatStartedThisActivity.getExtras().getParcelable("movieObject");
             fillMovieData(movie);
@@ -46,11 +61,14 @@ public class DetailsActivity extends AppCompatActivity {
     }
 
     /**
-     * Updates the UI for the Details Activity by setting the text
-     * and resources for the corresponding movie
-     * @param movie The movie the user clicked on
+     * Updates the Details Activity UI by setting the text
+     * and resources for the movie selected by the user
+     *
+     * @param movie The movie selected by the user
      */
     private void fillMovieData(Movie movie) {
+
+        // Movie data
         String movieTitle = movie.getMovieTitle();
         String posterPath = movie.getMoviePosterPath();
         Double voteAverage = movie.getMovieVoteAverage();
@@ -60,26 +78,54 @@ public class DetailsActivity extends AppCompatActivity {
         // Change activity label to be movie title
         setTitle(movieTitle);
 
-        // Load poster
-        Picasso.with(this)
-                .load(posterPath)
-                .placeholder(R.drawable.placeholder2)
-                .resize(200,250)
-                .error(R.drawable.movie_details_error)
-                .into(moviePosterView);
-
-        // Update textViews
-        movieVoteAverageView.setText(voteAverage.toString());
-        movieReleaseView.setText(releaseDate);
-        moviePlotView.setText(moviePlot);
-        movieTitleView.setText(movieTitle);
-
-
+        // Update views
+        loadMoviePoster(posterPath);
+        setViewData(movieVoteAverageView, voteAverage.toString());
+        setViewData(movieReleaseView, releaseDate);
+        setViewData(moviePlotView, moviePlot);
+        setViewData(movieTitleView, movieTitle);
     }
 
+    /**
+     * Load Movie poster into the corresponding view
+     * using the Picasso library.
+     *
+     * @param posterPath URL to fetch the movie poster
+     */
+    private void loadMoviePoster(String posterPath) {
+        if(posterPath != null) {
+            Picasso.with(this)
+                    .load(posterPath)
+                    .placeholder(R.drawable.placeholder2)
+                    .resize(200,300)
+                    .error(R.drawable.movie_details_error)
+                    .into(moviePosterView);
+        }
+    }
+
+    /**
+     * Extract the year the movie was released.
+     *
+     * @param releaseDate A String that represents
+     *                    a date with the format YYYY-MM-DD
+     * @return A four-digit year in String format
+     */
     private String extractReleaseYear(String releaseDate) {
-        String releaseYear = releaseDate.split("-")[0];
-        return releaseYear;
+        return releaseDate.split("-")[0];
     }
 
+    /**
+     * Sets the view's text to be the value provided
+     *
+     * @param view A TextView
+     * @param value A String that will be the text
+     *              for the view provided
+     */
+    private void setViewData(TextView view, String value) {
+        if(value != null) {
+            view.setText(value);
+        } else {
+            view.setText(NOT_AVAILABLE);
+        }
+    }
 }
